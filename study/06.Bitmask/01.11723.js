f0c72f4778eb6852dbce9efcc5c6e65caf19ea4f
PARK-PC\Park <junni34@ex-em.com>
2019-05-01 22:46:43 +0900

    비트연산 문제

---
+++ study/06.Bitmask/01.11723.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var input = require('fs').readFileSync(path.resolve('study/06.Bitmask/01.11723.txt'), 'utf8').toString().trim().split('\n');
+// var input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+// var S = input[0] + 1;
+var N = input;
+var NLen = input.length;
+var result = 0;
+var output = [];
+
+
+
+function typeCheck(type, num) {
+    switch (type) {
+        case "add":
+            result = result | (1 << num);
+            break;
+        case "check":
+            output.push(result & (1 << num) ? 1: 0);
+            break;
+        case "remove":
+            result = result & ~(1 << num);
+            break;
+        case "all":
+            var all_fn = function() {
+                var sum = 0;
+
+                for (var ix = 1; ix <= 20; ix++) {
+                    sum |= (1 << ix);
+                }
+                return sum;
+            }
+            result = all_fn();
+            break;
+        case "empty":
+            result = 0
+            break;
+        case "toggle":
+            result = result ^ (1 << num);
+            break;
+        default:
+            break;
+    }
+}
+
+for (var ix = 1; ix < NLen; ix++) {
+    var values = N[ix].split(" ");
+    typeCheck(values[0], values[1] * 1);
+}
+
+
+console.log(output.join("\n"));
---
+++ study/06.Bitmask/02.1182.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var input = require('fs').readFileSync(path.resolve('study/06.Bitmask/02.1182.txt'), 'utf8').toString().trim().split('\n');
+// var input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+var N = input[0].trim().split(" ")[0];
+var S = input[0].trim().split(" ")[1];
+
+var number = input[1].trim().split(" ");
+var cnt = 0
+var sum = 0;
+for (var ix = 1; ix < (1 << N); ix++) {
+    sum = 0;
+    for (var k = 0; k < N; k++) {
+        if ((ix & (1 << k))) {
+            sum += +(number[k]);
+        }
+    }
+    if (sum == S) {
+        cnt++;
+    }
+}
+
+console.log(cnt);
+
