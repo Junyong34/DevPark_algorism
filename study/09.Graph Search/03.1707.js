aeb0f168e859436a5cf03596e0ac16d345f6a254
PARK-PC\Park <junni34@ex-em.com>
2019-05-13 00:49:45 +0900

    백준 Node.js 버전 올려라 나쁜놈아

--- study/09.Graph Search/03.1707.js
+++ study/09.Graph Search/03.1707.js
@@ -1,95 +1,117 @@
-var path = require('path');
-var input = require('fs').readFileSync(path.resolve('study/09.Graph Search/03.1707.txt'), 'utf8').toString().trim().split('\n');
-// var input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+// var path = require('path');
+// var input = require('fs').readFileSync(path.resolve('study/09.Graph Search/03.1707.txt'), 'utf8').toString().trim().split('\n');
+var input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 var K = +input[0].trim().split(" ")[0];
 var isColor = [];
 var graphNum = [];
-var nodes = input.join("\n").trim();
-//
-// for (var ix = 1; ix < input.join("\n").trim().length; ix++) {
-//     var val = input.join("\n")[ix].trim();
-//     if (val !== "") {
-//         caseLen.push(val);
-//     }
-//
-// }
-if (nodes && nodes.length !== 0) {
-    var eLex = (nodes.split("\n").length - 1);
-    var LineMaxValue = Math.max.apply(null, nodes.split(""));
 
-    function initIsColor() {
-        graphNum = [];
-        for (var ix = 0; ix <= LineMaxValue; ix++) {
-            graphNum.push([]);
-            isColor[ix] = 0;
-        }
-    }
-
-    function sampleData(initVal, initLex) {
-        // console.log(initVal, initLex)
-        for (var ix = initVal; ix <= initLex; ix++) {
-
-            // var obj = [];
-            var a = +input[ix].trim().split(" ")[0];
-            var b = +input[ix].trim().split(" ")[1];
-            // console.log(a,b);
-            if (!graphNum[a]) {
-                graphNum[a] = [];
-            }
-            if (!graphNum[b]) {
-                graphNum[b] = [];
-            }
-            if (graphNum[a].indexOf(b) === -1) {
-                graphNum[a].push(b);
-            }
-
-            if (graphNum[b].indexOf(b) === -1) {
-                graphNum[b].push(a);
-            }
-        }
-    }
-
-    function dfs(start, cnt) {
-        isColor[start] = cnt;
-        for (var ix = 0; ix < graphNum[start].length; ix++) {
-            var tmp = graphNum[start][ix];
-            if (isColor[tmp] === 0) {
-                dfs(tmp, 3 - cnt);
-            }
-        }
-    }
-
-// // 테스트 케이스 수 만큼 실행시킨다.
-    var startIndex = 1;
-    var nextLet = eLex / K
-    var incre = eLex / K;
-    // if (K >= 2) {
-    while (K--) {
-        initIsColor();
-        sampleData(startIndex, nextLet);
-        for (var ix = 1; ix < LineMaxValue; ix++) {
-            if (isColor[ix] == 0) {
-                dfs(ix, 1);
-            }
-        }
-        var check = true;
-        for (var ix = 1; ix < LineMaxValue; ix++) {
-            if (!check) break;
-            for (var iy = 0; iy < graphNum[ix].length; iy++) {
-                var index = graphNum[ix][iy];
-                if (isColor[ix] === isColor[index]) {
-                    check = false;
-                    break;
-                }
-
-            }
-        }
-        console.log(check ? "YES" : "NO");
-        startIndex += incre;
-        nextLet += incre;
-    }
+function testCaseData(index, v, e) {
+    testIndex += e + 1;
+    isColor[0] = 0;
+    // for (var ix = 0; ix <= v; ix++) {
+    //     // graphNum.push([]);
+    //     isColor[ix] = 0;
     // }
+    graphNum = [];
+    // console.log(initVal, initLex)
+    for (var ix = 0; ix <= e; ix++) {
 
+        if (!ix) {
+            graphNum[ix] = [];
+            graphNum[ix].push(0);
+            continue;
+        }
+        // var obj = [];
+        var a = +input[ix + index].trim().split(" ")[0];
+        var b = +input[ix + index].trim().split(" ")[1];
+        // console.log(a,b);
+        if (!graphNum[a]) {
+            graphNum[a] = [];
+        }
+        if (!graphNum[b]) {
+            graphNum[b] = [];
+        }
+        // if (graphNum[a].indexOf(b) === -1) {
+            graphNum[a].push(b);
+        // }
 
+        // if (graphNum[b].indexOf(b) === -1) {
+            graphNum[b].push(a);
+        // }
+    }
 }
+
+
+// if (nodes && nodes.length !== 0) {
+//     // var eLex = (nodes.split("\n").length - 1);
+//     //
+//
+//
+function dfs(start) {
+    // isColor[start] = cnt;
+    // if(graphNum[start] == null) return;
+    // console.log(start, "@@@");
+    if(isColor[start] == null) {
+        isColor[start] = 0;
+    }
+    var arrayLen =  [...new Set(graphNum[start])];
+    for (var ix = 0; ix < arrayLen.length; ix++) {
+        var tmp = graphNum[start][ix];
+        if(isColor[tmp] == null) {
+            isColor[tmp] = 0;
+        }
+        if (isColor[tmp] !== 0) {
+            // dfs(tmp, 3 - cnt);
+            if (isColor[start] === isColor[tmp]) {
+                check = false;
+                return
+            }
+        } else {
+            if (isColor[start] === 1) {
+                isColor[tmp] = 2;
+                dfs(tmp);
+            } else {
+                isColor[tmp] = 1;
+                dfs(tmp);
+            }
+        }
+    }
+}
+
+//
+// // // 테스트 케이스 수 만큼 실행시킨다.
+// if (K >= 2) {
+var testIndex = 1;
+// while (K--) {
+for (var iz = 1; iz <= K; iz++) {
+    var V = +input[testIndex].trim().split(" ")[0];
+    var E = +input[testIndex].trim().split(" ")[1];
+    isColor = [];
+    testCaseData(testIndex, V, E);
+    var check = true;
+    for (var ix = 1; ix <= V; ix++) {
+        if (isColor[ix] == 0 || isColor[ix] == null) {
+            isColor[ix] = 1;
+            dfs(ix);
+        }
+        if (check === false) break;
+    }
+
+    // for (var ix = 1; ix <= V; ix++) {
+    //     if (!check) break;
+    //     for (var iy = 0; iy < graphNum[ix].length; iy++) {
+    //         var index = graphNum[ix][iy];
+    //         if (isColor[ix] === isColor[index]) {
+    //             check = false;
+    //             break;
+    //         }
+    //
+    //     }
+    // }
+    console.log(check ? "YES" : "NO");
+}
+// }
+
+
+// }
