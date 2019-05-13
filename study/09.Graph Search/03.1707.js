e804dbbdbdf3715c256c57c2e7749749b536c8f5
Junyong <junni34@ex-em.com>
2019-05-13 20:23:41 +0900

    Graph Search

--- study/09.Graph Search/03.1707.js
+++ study/09.Graph Search/03.1707.js
@@ -33,11 +33,11 @@ function testCaseData(index, v, e) {
             graphNum[b] = [];
         }
         // if (graphNum[a].indexOf(b) === -1) {
-            graphNum[a].push(b);
+        graphNum[a].push(b);
         // }
 
         // if (graphNum[b].indexOf(b) === -1) {
-            graphNum[b].push(a);
+        graphNum[b].push(a);
         // }
     }
 }
@@ -48,34 +48,64 @@ function testCaseData(index, v, e) {
 //     //
 //
 //
-function dfs(start) {
-    // isColor[start] = cnt;
-    // if(graphNum[start] == null) return;
-    // console.log(start, "@@@");
-    if(isColor[start] == null) {
-        isColor[start] = 0;
-    }
-    var arrayLen =  [...new Set(graphNum[start])];
-    for (var ix = 0; ix < arrayLen.length; ix++) {
-        var tmp = graphNum[start][ix];
-        if(isColor[tmp] == null) {
-            isColor[tmp] = 0;
-        }
-        if (isColor[tmp] !== 0) {
-            // dfs(tmp, 3 - cnt);
-            if (isColor[start] === isColor[tmp]) {
-                check = false;
-                return
+// function dfs(start) {
+//     // isColor[start] = cnt;
+//     // if(graphNum[start] == null) return;
+//     // console.log(start, "@@@");
+//     // if(isColor[start] == null) {
+//     //     isColor[start] = 0;
+//     // }
+//     // var arrayLen =  [...new Set(graphNum[start])];
+//     if(graphNum[start] == null) return;
+//     for (var ix = 0; ix < graphNum[start].length; ix++) {
+//         var tmp = graphNum[start][ix];
+//         if (isColor[tmp] == null) {
+//             isColor[tmp] = 0;
+//         }
+//         if (isColor[tmp] !== 0) {
+//             // dfs(tmp, 3 - cnt);
+//             if (isColor[start] === isColor[tmp]) {
+//                 check = false;
+//                 return
+//             }
+//         } else {
+//             if (isColor[start] === 1) {
+//                 isColor[tmp] = 2;
+//                 dfs(tmp);
+//             } else {
+//                 isColor[tmp] = 1;
+//                 dfs(tmp);
+//             }
+//         }
+//     }
+// }
+
+
+var queue = [];
+
+function bfs(start) {
+    var c = 1;
+    queue.push(start);
+    isColor[start] = c;
+
+    while (queue.length !== 0) {
+        var index = queue.shift();
+        c = 3 - isColor[index];
+        if (graphNum[index]) {
+            for (var ix = 0; ix < graphNum[index].length; ix++) {
+                var next = graphNum[index][ix];
+
+                if (isColor[index] === isColor[next]) {
+                    check = false;
+                    return
+                }
+                if (!isColor[next]) {
+                    queue.push(next);
+                    isColor[next] = c;
+                }
             }
-        } else {
-            if (isColor[start] === 1) {
-                isColor[tmp] = 2;
-                dfs(tmp);
-            } else {
-                isColor[tmp] = 1;
-                dfs(tmp);
-            }
         }
+
     }
 }
 
@@ -84,33 +114,43 @@ function dfs(start) {
 // if (K >= 2) {
 var testIndex = 1;
 // while (K--) {
-for (var iz = 1; iz <= K; iz++) {
-    var V = +input[testIndex].trim().split(" ")[0];
-    var E = +input[testIndex].trim().split(" ")[1];
-    isColor = [];
-    testCaseData(testIndex, V, E);
-    var check = true;
-    for (var ix = 1; ix <= V; ix++) {
-        if (isColor[ix] == 0 || isColor[ix] == null) {
-            isColor[ix] = 1;
-            dfs(ix);
-        }
-        if (check === false) break;
-    }
+if (K >= 2 && K <= 5) {
+    for (var iz = 1; iz <= K; iz++) {
+        var V = +input[testIndex].trim().split(" ")[0];
+        var E = +input[testIndex].trim().split(" ")[1];
+        if (V >= 1 && E >= 1) {
+            isColor = [];
+            testCaseData(testIndex, V, E);
 
-    // for (var ix = 1; ix <= V; ix++) {
-    //     if (!check) break;
-    //     for (var iy = 0; iy < graphNum[ix].length; iy++) {
-    //         var index = graphNum[ix][iy];
-    //         if (isColor[ix] === isColor[index]) {
-    //             check = false;
-    //             break;
-    //         }
-    //
-    //     }
-    // }
-    console.log(check ? "YES" : "NO");
+            var check = true;
+
+            for (var ix = 1; ix <= V; ix++) {
+                if (!isColor[ix]) {
+                    // isColor[ix] = 1;
+                    // dfs(ix);
+                    bfs(ix);
+                }
+                if (check === false) break;
+            }
+
+            // for (var ix = 1; ix <= V; ix++) {
+            //     if (!check) break;
+            //     for (var iy = 0; iy < graphNum[ix].length; iy++) {
+            //         var index = graphNum[ix][iy];
+            //         if (isColor[ix] === isColor[index]) {
+            //             check = false;
+            //             break;
+            //         }
+            //
+            //     }
+            // }
+            queue.length = 0;
+            console.log(check ? "YES" : "NO");
+        }
+
+    }
 }
+
 // }
 
 
